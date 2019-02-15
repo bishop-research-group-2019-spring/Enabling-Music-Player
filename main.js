b9191033b132d0f85edb7e6ed36d09ae32ea31e8
Andrew McKinnon <andrewmckinnon2@gmail.com>
2019-02-15 12:53:22 -0500

    Cleaned interface slightly and added keyboard inputs

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const player = new Player();
 
 //to play note use 'player.playNoteDown(pitch) where pitch is an int
 
+//bind keyboard notes to clicking of large buttons on web page
 $("#top-left-button").click(() => {
     player.playNoteDown(100);
 })
@@ -18,5 +19,30 @@ $("#bottom-left-button").click(() => {
 }) 
 
 $("#bottom-right-button").click(() => {
-    player.playNoteDown(96);
+    player.playNoteDown(95);
+})
+
+
+//bind notes to pressing of keys (S,D,X,C)
+$(document).keydown((e) => {
+    console.log("code of button press:");
+    console.log(e.which);
+
+    switch(e.which){
+        case 83: //S
+            player.playNoteDown(100);
+        break;
+
+        case 68: //D
+            player.playNoteDown(102);
+        break;
+
+        case 88: //X
+            player.playNoteDown(90);
+        break;
+
+        case 67: //C
+            player.playNoteDown(95);
+        break;
+    }
 })
